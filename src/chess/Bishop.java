e2f0f4a56788fe2005ab09fdfe178e96420f0f11
Jose <josevercas44@gmail.com>
2024-02-23 11:19:55 +0100

    new classes

---
+++ src/chess/Bishop.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chess;
+
+/**
+ *
+ * @author jose
+ */
+public class Bishop extends Piece{
+
+    public Bishop(boolean isWhite, int row, int col) {
+        super(isWhite, row, col);
+    }
+    
+    @Override
+    public void move() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
